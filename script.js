8b5f2aea9fbff3e95f0aa98ff761c74cd70a31d1
Nassim <nassim.haddad.pro@gmail.com>
2021-02-05 15:59:22 +0100

    Rain drop simulation with JS and CSS animations

---
+++ script.js
@@ -0,0 +1,18 @@
+const body = document.body;
+
+setInterval(rainFall, 10);
+function rainFall(){
+    const waterDrop = document.createElement('i');
+
+    waterDrop.classList.add('fas', 'fa-tint');
+    waterDrop.style.fontSize = Math.floor(Math.random() * 7) + 'px';
+    waterDrop.style.animationDuration = Math.random() * 2 + 's';
+    waterDrop.style.opacity = Math.random() + 0.3;
+    waterDrop.style.left = Math.random() * window.innerWidth + 'px';
+
+    body.appendChild(waterDrop);
+
+    setTimeout(() => {
+        waterDrop.remove();
+    }, 5000)
+}
